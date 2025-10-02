647be1e134e57e5f50bc55c35896fbf35ca91ebe
gimdongwon <ehddnjs8989@gmail.com>
2025-10-02 21:28:50 +0900

    (fix) 비정상 태그 노출 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,33 +51,24 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <Script id='google-tag-manager'>
-          {`
-        // Google Tag Manager
-        <Script id='google-tag-manager-noscript'>
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script id='google-tag-manager' strategy='afterInteractive'>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PVQVPLJC');
-// End Google Tag Manager
-          `}
+})(window,document,'script','dataLayer','GTM-PVQVPLJC');`}
         </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} body`}>
-        <Providers>{children}</Providers>
-        {`
-        // Google Tag Manager (noscript)
         <noscript>
           <iframe
             src='https://www.googletagmanager.com/ns.html?id=GTM-PVQVPLJC'
             height='0'
             width='0'
-            style='display:none;visibility:hidden'
-          ></iframe>
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
         </noscript>
-        // End Google Tag Manager (noscript)
-        `}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
