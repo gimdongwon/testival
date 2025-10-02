965fe8e337d875581c2e72a8977766ede4055013
gimdongwon <ehddnjs8989@gmail.com>
2025-10-02 20:39:01 +0900

    (feat) og 이미지, 텍스트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,33 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
+const siteUrl = 'https://testival.kr';
+const defaultTitle = 'Testival';
+const defaultDesc = 'Testival';
+const defaultImage = `${siteUrl}/images/quiz/chuseok/ogImage.png`;
+
 export const metadata: Metadata = {
-  title: 'Testival',
-  description: 'Testival',
+  metadataBase: new URL(siteUrl),
+  title: { default: defaultTitle, template: '%s | 서비스 이름' },
+  description: defaultDesc,
+  openGraph: {
+    type: 'website',
+    url: siteUrl,
+    siteName: '서비스 이름',
+    title: defaultTitle,
+    description: defaultDesc,
+    locale: 'ko_KR',
+    images: [
+      { url: defaultImage, width: 1200, height: 630, alt: '미리보기 이미지' },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: defaultTitle,
+    description: defaultDesc,
+    images: [defaultImage],
+  },
+  alternates: { canonical: siteUrl },
 };
 
 export default function RootLayout({
--- src/app/quiz/[id]/result/page.tsx
+++ src/app/quiz/[id]/result/page.tsx
@@ -1,7 +1,37 @@
 // app/quiz/[id]/result/page.tsx
 import ResultClient from './result.client';
-
+import type { Metadata } from 'next';
+type Props = { params: { id: string } };
 export const revalidate = 600;
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = params;
+  const SITE_URL = 'https://testival.kr';
+  const title = 'Testival 결과 페이지';
+  const desc = 'Testival 결과 페이지';
+  const img = `${SITE_URL}/images/quiz/chuseok/ogResult.png`;
+
+  return {
+    title,
+    description: desc,
+    openGraph: {
+      type: 'article',
+      siteName: '서비스 이름',
+      url: `${SITE_URL}/quiz/${id}/result`,
+      title,
+      description: desc,
+      images: [{ url: img, width: 1200, height: 630 }],
+      locale: 'ko_KR',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description: desc,
+      images: [img],
+    },
+    alternates: { canonical: `${SITE_URL}/quiz/${id}/result` },
+  };
+}
 
 export default async function Page() {
   return <ResultClient />;
