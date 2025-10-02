c3cf42d521f14f3b37523d3089387cb796f9c91f
gimdongwon <ehddnjs8989@gmail.com>
2025-10-02 21:16:23 +0900

    (feat) ga 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Providers from './providers';
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -49,8 +50,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <Script id='google-tag-manager'>
+          {`
+        // Google Tag Manager
+        <Script id='google-tag-manager-noscript'>
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PVQVPLJC');
+// End Google Tag Manager
+          `}
+        </Script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} body`}>
         <Providers>{children}</Providers>
+        {`
+        // Google Tag Manager (noscript)
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-PVQVPLJC'
+            height='0'
+            width='0'
+            style='display:none;visibility:hidden'
+          ></iframe>
+        </noscript>
+        // End Google Tag Manager (noscript)
+        `}
       </body>
     </html>
   );
